e72cdc126ba64ab66f1863171969afcc319ff8b0
Gary Houston <ghouston@arglist.com>
2016-01-12 15:32:02 +1100

    Give more info when response data limit is exceeded.

--- takenwith.go
+++ takenwith.go
@@ -241,6 +241,7 @@ func processFiles(fileArray []exifcamera.FileCamera, client *mwclient.Client, fl
 }
 
 func processGenerator(params params.Values, client *mwclient.Client, flags flags, categoryMap map[string]string, allCategories map[string]bool, catCounts map[string]int32) {
+	lastFileProcessed := ""
 	query := client.NewQuery(params)
 	for query.Next() {
 		json := query.Resp()
@@ -274,11 +275,23 @@ func processGenerator(params params.Values, client *mwclient.Client, flags flags
 				}
 				idx++
 			}
+			lastFileProcessed = pageArray[len(pageArray)-1].Title
 			processFiles(pageArray, client, flags, categoryMap, allCategories, catCounts)
 		}
 	}
 	if query.Err() != nil {
-		panic(query.Err())
+		fmt.Println("Last file processed: ", lastFileProcessed)
+		msg := query.Err().Error()
+		if strings.Contains(msg, "This result was truncated") {
+			// This is a nuisence. Some files have enormous Exif
+			// data and the batch may exceed a limit of
+			// 12,582,912 bytes for a single request.
+			// E.g., File:Congressional Record Volume 81 Part 1.pdf
+			fmt.Println(msg)
+			fmt.Println("The combined Exif data was too large when processing a batch of files. Try running from the time of the last file processed with a smaller -batchSize.")
+		} else {
+			panic(query.Err())
+		}
 	}
 }
 
