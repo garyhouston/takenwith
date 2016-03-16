be5fe05f82a418f0810b2066550dc675e74c8677
Gary Houston <ghouston@arglist.com>
2016-03-16 16:09:42 +1100

    read username / password from stdin instead of cmdline, take user info from config file.

--- wikilogin/login.go
+++ wikilogin/login.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/vharitonsky/iniflags"
 	mwclient "cgt.name/pkg/go-mwclient"
 	"github.com/garyhouston/takenwith/mwlib"
 	"log"
 	"os"
 )
 
+// Flags are just to get the user details, usually from the configuration file.
+func getUser() string {
+	iniflags.SetConfigFile(mwlib.GetWorkingDir() + "/wikilogin.conf")
+	var user string
+	flag.StringVar(&user, "user", "nobody@example.com", "Operator's email address or Wiki user name.")
+	iniflags.Parse()
+	return user
+}
+
 // This login program must be run before using the main bot. It saves
 // cookies into a file in the bot's directory.
 func main() {
-	if len(os.Args) != 4 {
-		log.Fatal("Usage: ", os.Args[0], " username password operator@email")
+	userinfo := getUser()
+	if len(os.Args) != 1 {
+		log.Fatal("Usage: ", os.Args[0], " -help: display options")
 	}
-	client, err := mwclient.New("https://commons.wikimedia.org/w/api.php", "wikilogin "+os.Args[3])
+	client, err := mwclient.New("https://commons.wikimedia.org/w/api.php", "wikilogin " + userinfo)
 	if err != nil {
 		panic(err)
 	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Username: ")
+	scanner.Scan()
+	user := scanner.Text()
+	fmt.Print("Password: ")
+	scanner.Scan()
+	password := scanner.Text()
+	
 	client.Maxlag.On = true
 
-	err = client.Login(os.Args[1], os.Args[2])
+	err = client.Login(user, password)
 	if err != nil {
 		log.Fatal(err)
 	}
