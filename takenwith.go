6be228c9d3974cf1ad4bd5e3f5bd8e5a2c6def99
Gary Houston <ghouston@arglist.com>
2016-08-28 13:26:09 +1000

    fix spacing in a warning

--- takenwith.go
+++ takenwith.go
@@ -219,7 +219,7 @@ func mapCategory(pageObj *jason.Object, verbose func(...string), categoryMap map
 		return fileTarget{}, false
 	}
 	if strings.HasPrefix(catMapped, "skip ") {
-		verbose(title, "\n", "Skipping ", make, model)
+		verbose(title, "\n", "Skipping ", make, " ", model)
 		return fileTarget{}, false
 	}
 	if catMapped == "Category:CanonS100 (special case)" {
