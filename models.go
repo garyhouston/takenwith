bad3ce0f8668e8654577b27e79b618e44be04ed2
Gary Houston <ghouston@arglist.com>
2016-09-06 20:51:58 +1000

    Initial work for a regex matcher.

--- models.go
+++ models.go
@@ -3,33 +3,19 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"strings"
 )
 
-func convertCategory(field string) string {
-	if strings.HasPrefix(field, "Category:") {
-		// use name as-is.
-		return field
-	} else if strings.HasPrefix(field, "Taken with") {
-		// avoid accidental "Taken with Taken with".
-		panic(fmt.Sprintf("Bad record in category mapping file: %v", field))
-	} else {
-		return "Category:Taken with " + field
-	}
-}
-
-// fill map with relations of makemodel -> Commons category
-func fillCategoryMap(mappingFile string) map[string]string {
+func readCSV(mappingFile string, convert func(record []string)) {
 	file, err := os.Open(mappingFile)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 	reader := csv.NewReader(bufio.NewReader(file))
-	var categories = make(map[string]string)
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
@@ -38,14 +24,52 @@ func fillCategoryMap(mappingFile string) map[string]string {
 		if err != nil {
 			panic(err)
 		}
-		categories[record[0]+record[1]] = convertCategory(record[2])
+		convert(record)
 	}
+}
+
+// Fill map with relations of makemodel -> Commons category
+func fillCategoryMap(mappingFile string) map[string]string {
+	categories := make(map[string]string)
+	convert := func(record []string) {
+		var mapped string
+		if strings.HasPrefix(record[2], "Category:") {
+			// use name as-is.
+			mapped = record[2]
+		} else if strings.HasPrefix(record[2], "Taken with") {
+			// avoid accidental "Taken with Taken with".
+			panic("Bad record in category mapping file: " + record[2])
+		} else {
+			mapped = "Category:Taken with " + record[2]
+		}
+		categories[record[0]+record[1]] = mapped
+	}
+	readCSV(mappingFile, convert)
 	return categories
+}
+
+type catRegex struct {
+	regex  *regexp.Regexp
+	target string
+}
+
+// Read regular expressions for category matches.
+func fillRegex(regexFile string) []catRegex {
+	regexes := make([]catRegex, 0, 200)
+	convert := func(record []string) {
+		regex, err := regexp.Compile(record[0])
+		if err != nil {
+			panic(err)
+		}
+		regexes = append(regexes, catRegex{regex, record[1]})
+	}
+	readCSV(regexFile, convert)
+	return regexes
 }
 
 // Fill the complete set of relevant Commons Categories.
 func fillCategories(categoryMap map[string]string, exceptionFile string) map[string]bool {
-	var categories = make(map[string]bool)
+	categories := make(map[string]bool)
 	for _, v := range categoryMap {
 		categories[v] = true
 	}
